137ef2578c08266ff50e22776dbee184cbe39bf6
luisperaza2021 <luis.peraza@catolica.edu.sv>
2021-05-10 13:47:50 -0600

    database

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,7 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('libros', function () {
+    return response()->json(Libro::all(), 200);
+});
